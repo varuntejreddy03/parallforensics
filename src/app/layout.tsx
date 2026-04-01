b4190c1665a9bc8b08a9e0c4db916ce26cf5febb
VarunTej <varuntejreddy03@gmail.com>
2026-04-02 00:52:38 +0530

    chore: added manifest and all favicon/app icons to layout metadata

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -31,6 +31,17 @@ export const metadata: Metadata = {
   },
   alternates: {
     canonical: "https://parallforensics.com"
+  },
+  manifest: "/site.webmanifest",
+  icons: {
+    icon: [
+      { url: "/favicon.ico" },
+      { url: "/favicon-16x16.png", sizes: "16x16", type: "image/png" },
+      { url: "/favicon-32x32.png", sizes: "32x32", type: "image/png" },
+    ],
+    apple: [
+      { url: "/apple-touch-icon.png", sizes: "180x180", type: "image/png" }
+    ],
   }
 };
 
